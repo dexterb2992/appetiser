8140102f039a1d741217df70c32822db75d5eb5c
Dexter Bengil <dexterb2992@gmail.com>
2018-10-08 03:49:53 +0800

    Initial commit

---
+++ components/RegistrationForm.vue
@@ -0,0 +1,58 @@
+<template>
+	<div class="columns">
+		<div class="column">
+			<div class="field">
+				<label class="label">Name</label>
+				<div class="control">
+					<input class="input" type="text" placeholder="Text input">
+				</div>
+			</div>
+
+			<div class="field">
+				<label class="label">Email</label>
+				<div class="control has-icons-left has-icons-right">
+					<input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+					<span class="icon is-small is-left">
+						<i class="fas fa-envelope"></i>
+					</span>
+					<span class="icon is-small is-right">
+						<i class="fas fa-exclamation-triangle"></i>
+					</span>
+				</div>
+				<p class="help is-danger">This email is invalid</p>
+			</div>
+
+			<div class="field">
+				<label class="label">Password</label>
+				<div class="control has-icons-left has-icons-right">
+					<input class="input is-danger" type="email" placeholder="Email input" value="hello@">
+					<span class="icon is-small is-left">
+						<i class="fas fa-envelope"></i>
+					</span>
+					<span class="icon is-small is-right">
+						<i class="fas fa-exclamation-triangle"></i>
+					</span>
+				</div>
+				<p class="help is-danger">This email is invalid</p>
+			</div>
+
+			<div class="field is-grouped">
+				<div class="control">
+					<button class="button is-link">Submit</button>
+				</div>
+				<div class="control">
+					<button class="button is-text">Cancel</button>
+				</div>
+			</div>
+		</div>	
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			hasError: false,
+			errorMessage: null
+		}
+	}
+</script>
